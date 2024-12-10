7a9e74a9a8af692b63c6868f2604e2aa40832898
DaynaFS <yazmin.fs9194@gmail.com>
2024-12-10 00:27:00 -0600

    cambios en finales de linea

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ import App from './frontend/App';
 //import { name as SalaperMovil } from './app.json';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
+// It also ensures that whether yoau load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
